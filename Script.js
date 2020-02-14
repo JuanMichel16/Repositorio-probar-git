a58e504467e1319ece885937fb9da7319a17e6b5
Juan Michel <juan.michel16@outlook.com>
2020-02-14 00:18:40 -0600

    Probando rama test

--- Script.js
+++ Script.js
@@ -45,3 +45,12 @@ for(i=0;i<10;i+=1){
 for(i=0;i<10;i+=1){
     console.log("kyc prra")
 }
+for(i=0;i<10;i+=1){
+    console.log("kyc prra")
+}
+for(i=0;i<10;i+=1){
+    console.log("kyc prra")
+}
+for(i=0;i<10;i+=1){
+    console.log("kyc prra")
+}
